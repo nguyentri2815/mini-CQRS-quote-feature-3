91fa850a2c2d80e168c12979897538c62c0219f8
Tri Nguyen <tri.nguyen@alpaca.vn>
2026-05-17 21:20:21 +0700

    first commit

---
+++ src/main/java/com/example/quote_service_eventstore/QuoteServiceEventstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.quote_service_eventstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteServiceEventstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteServiceEventstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quote_service_eventstore/QuoteServiceEventstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quote_service_eventstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceEventstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
